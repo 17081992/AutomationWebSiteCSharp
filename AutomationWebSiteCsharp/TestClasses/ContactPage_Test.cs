de761dc89a2e27698e01cb09cb448378e8a3289a
SebastianM25 <h.sebiastian@gmail.com>
2021-05-22 22:54:48 +0300

    Add project files.

---
+++ AutomationWebSiteCsharp/TestClasses/ContactPage_Test.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationWebSiteCsharp.TestClasses
+{
+    class ContactPage_Test
+    {
+    }
+}
---
+++ AutomationWebSiteCsharp/UIElements/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace AutomationWebSiteCsharp
+{
+    class Contact
+    {
+        private IWebDriver driver;
+
+        public Contact(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        [FindsBy(How = How.XPath, Using = "//a[contains(text(),'Contact')]")]
+        private IWebElement contact;
+        public IWebElement ContactUs() { return contact; }
+
+    }
+}
